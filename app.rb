398530a00a5ddec18d4eea8a4cf6ec6f2a83f97c
Martyn <you@example.com>
2021-01-18 12:34:46 +0300

    Add parametrs validation

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get '/' do
 end
 
 get '/About' do
+	@error = 'something wrong'
 	erb :about
 end
 
@@ -22,6 +23,30 @@ post '/visit' do
 	@datetime = params[:datetime]
 	@barber   = params[:barber]
 	@color    = params[:color]
+
+	if @username == ''
+		@error = 'Введите имя'
+		return erb :visit
+	end
+
+	if @phone == ''
+		@error = 'Введите телефон'
+		return erb :visit
+	end
+
+	if @datetime == ''
+		@error = 'Введите время'
+		return erb :visit
+	end
+	if @barber == ''
+		@error = 'Введите имя парикхмахера'
+		return erb :visit
+	end
+	if @color == ''
+		@error = 'Введите цвет'
+		return erb :visit
+	end
+
 
 	erb "OK, #{@username}, #{@phone}, #{@datetime}, #{@barber}"
 end
